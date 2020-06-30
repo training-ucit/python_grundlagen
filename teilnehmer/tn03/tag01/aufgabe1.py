a6d18267af938815a8baa95add055bb37c3e6d09
TN03 <tn03@kurs.ucit>
2020-06-30 13:53:11 +0200

    added docstring and corrected some lines to get 100% @pylint

--- teilnehmer/tn03/tag01/aufgabe1.py
+++ teilnehmer/tn03/tag01/aufgabe1.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 
+"""This script prompts a user to enter a number and print that out until stop is given"""
+
 while True:
-     eingabe = input ("Bitte ne Zahl (oder stop für ENDE): ")
-     if eingabe == "stop":
-         break
-     else:
-#         print("Eingabe: " + eingabe) 
-         if eingabe.isdigit():
-             print("4711 + " + eingabe + " = " + str(4711+int(eingabe)))
-         else:
-             print("'"+ eingabe + "'" + " ist nicht wirklich ne Zahl")    
-
-
-
+    eingabe = input("Bitte ne Zahl (oder stop für ENDE): ")
+    if eingabe == "stop":
+        break
+    if eingabe.isdigit():
+        print("4711 + " + eingabe + " = " + str(4711+int(eingabe)))
+    else:
+        print("'"+ eingabe + "'" + " ist nicht wirklich ne Zahl")
