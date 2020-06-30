ad4bd691478b3efc6d65cb5acc38f4dc7abbb9e9
TN03 <tn03@kurs.ucit>
2020-06-30 13:37:18 +0200

    added aufgabe1.py

--- teilnehmer/tn03/tag01/aufgabe1.py
+++ teilnehmer/tn03/tag01/aufgabe1.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
 while True:
-     eingabe = input ("Etwas : ")
+     eingabe = input ("Bitte ne Zahl (oder stop für ENDE): ")
      if eingabe == "stop":
          break
      else:
-         print("Eingabe: " + eingabe) 
+#         print("Eingabe: " + eingabe) 
+         if eingabe.isdigit():
+             print("4711 + " + eingabe + " = " + str(4711+int(eingabe)))
+         else:
+             print("'"+ eingabe + "'" + " ist nicht wirklich ne Zahl")    
 
 
- print("*"*20)        
 
- for 
